6891061adab6842c3e1cd7e6f89a60f9c404a225
aizul <aizulhussin@gmail.com>
2020-03-17 17:12:21 +0000

    done deleteTodos

--- backend/src/lambda/http/deleteTodo.ts
+++ backend/src/lambda/http/deleteTodo.ts
@@ -1,10 +1,35 @@
 import 'source-map-support/register'
-
+import * as AWS  from 'aws-sdk'
 import { APIGatewayProxyEvent, APIGatewayProxyResult, APIGatewayProxyHandler } from 'aws-lambda'
+
+const todosTable = process.env.TODOS_TABLE
+const docClient = new AWS.DynamoDB.DocumentClient()
 
 export const handler: APIGatewayProxyHandler = async (event: APIGatewayProxyEvent): Promise<APIGatewayProxyResult> => {
   const todoId = event.pathParameters.todoId
+  
+  var params = {
+    TableName:todosTable,
+    Key:{
+        "todoId": todoId,
+    }
+  };
+  
+  docClient.delete(params, function(err, data) {
+  if (err) {
+    console.log(err, err.stack);
+  }
+  else {
+    console.log(data);
+    
+  }
+});
 
-  // TODO: Remove a TODO item by id
-  return undefined
+  return {
+    statusCode: 200,
+    headers: {
+      'Access-Control-Allow-Origin': '*'
+    },
+    body:JSON.stringify({})
+  }
 }
