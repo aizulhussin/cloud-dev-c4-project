2bbab790606cd011ae1498dbc250b42d6659ff37
aizul <aizulhussin@gmail.com>
2020-03-17 16:46:26 +0000

    done createToDo

--- backend/src/lambda/http/createTodo.ts
+++ backend/src/lambda/http/createTodo.ts
@@ -2,13 +2,12 @@ import 'source-map-support/register'
 import * as AWS  from 'aws-sdk'
 import * as uuid from 'uuid'
 import { APIGatewayProxyEvent, APIGatewayProxyHandler, APIGatewayProxyResult } from 'aws-lambda'
-import {getUserId} from '../../lambda/utils' 
+import { getUserId } from '../../lambda/utils' 
 import { CreateTodoRequest } from '../../requests/CreateTodoRequest'
+
 const docClient = new AWS.DynamoDB.DocumentClient()
 
 const todosTable = process.env.TODOS_TABLE
-const bucketName = process.env.IMAGES_S3_BUCKET
-const urlExpiration = process.env.SIGNED_URL_EXPIRATION
 
 export const handler: APIGatewayProxyHandler = async (event: APIGatewayProxyEvent): Promise<APIGatewayProxyResult> => {
   
@@ -33,6 +32,8 @@ async function createToDo(userId:string,todoId:string,createTodoRequest:CreateTo
   
   const timestamp = new Date().toISOString()
   const done = false;
+  const imageS3Url = ``
+  
   
   const newItem = {
     userId,
@@ -40,6 +41,7 @@ async function createToDo(userId:string,todoId:string,createTodoRequest:CreateTo
     timestamp,
     done,
     createTodoRequest,
+    imageS3Url
   }
   
   await docClient
